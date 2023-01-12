0a9f9d46718abead0068cf5b2b41cc3c9aec53e1
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-01-12 17:36:10 +0100

    add pop-up con punteggio e varie interazioni

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,3 @@
-// funzione ciclo
-function myCicle (val1, val2, val3) {
-
-    gridContainer.innerHTML = '';
-
-    bomb.length = 0;
-
-    for (let i = val1; i <= val2; i++) {
-
-        const myCell = document.createElement('div');
-    
-        myCell.classList.add(val3);
-    
-        const myElement = document.createElement('span');
-    
-        myElement.classList.add('none');
-    
-        myCell.append(myElement);
-    
-        // myElement.append(i);
-    
-        gridContainer.append(myCell);
-    
-        myCell.addEventListener('click',
-    
-            function () {
-
-                if (bomb.includes(i)) {
-
-                    myCell.classList.add('bomb');
-
-                } else {
-
-                    myCell.classList.add('cell-color');
-
-                }
-                
-                myElement.classList.toggle('none');
-    
-                console.log(i);
-    
-            }
-    
-        );
-    
-    };
-
-};
-
-//funzione random number
-function randomNumber (min ,max) {
-    
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-
-};
-
-function myRandomNumberBomb (num1, num2, myArray) {
-
-    for (let i = 1; i <= 16; i++) {
-
-        let myRandomNumber = randomNumber (num1 ,num2);
-    
-        while (bomb.includes(myRandomNumber)) {
-    
-            myRandomNumber = randomNumber (num1 ,num2);
-    
-        };
-    
-        myArray.push(myRandomNumber);
-    
-        console.log(myRandomNumber)
-    
-    };
-
-};
-
 //intro
 const intro = document.getElementById('p-intro');
 
@@ -97,14 +21,47 @@ const playButton = document.getElementById('playgame');
 // array delle bombe 
 const bomb = [];
 
-// creazione celle
+// verifica per rendere non più cliccabili le celle
+let verify = 0
+
+// contenitore dei punti
+const containerPoints = document.getElementById('points');
+
+containerPoints.classList.add('none');
+
+// punti
+const myPoints = document.getElementById('my-points');
+
+//contatore dei punti 
+let counter = 0
+
+//end game
+const endGameCont = document.getElementById('end-game');
+
+endGameCont.classList.add('none');
+
+//punti finali
+const finalPoints = document.getElementById('final-points');
+
+//overlay end game
+const overlayEndGame = document.getElementById('overlay-endgame');
+
+overlayEndGame.classList.add('none');
+
+// bottone play
 playButton.addEventListener('click',
 
     function () {
 
+        gridContainer.classList.remove('none');
+
         gridContainer.classList.add('flex');
 
         intro.classList.add('none');
+
+        containerPoints.classList.remove('none');
+
+        containerPoints.classList.add('block');
 
         let difficulty = mySelect.value; 
 
@@ -131,3 +88,117 @@ playButton.addEventListener('click',
     }
 
 );
+
+// bottone gioca ancora
+const playAgain = document.getElementById('play-again');
+
+playAgain.addEventListener('click',
+
+    function () {
+
+        location.reload();
+
+    }
+
+);
+
+/*
+
+----- FUNZIONI ------
+
+*/
+
+// funzione ciclo
+function myCicle (val1, val2, val3) {
+
+    gridContainer.innerHTML = '';
+
+    bomb.length = 0;
+
+    for (let i = val1; i <= val2; i++) {
+
+        const myCell = document.createElement('div');
+    
+        myCell.classList.add(val3);
+    
+        const myElement = document.createElement('span');
+    
+        myElement.classList.add('none');
+    
+        myCell.append(myElement);
+    
+        gridContainer.append(myCell);
+    
+        myCell.addEventListener('click',
+    
+            function () {
+
+                if (verify == 0) {
+
+                    if (bomb.includes(i)) {
+
+                        myCell.classList.add('bomb');
+    
+                        overlayEndGame.classList.remove('none')
+    
+                        endGameCont.classList.remove('none');
+    
+                        endGameCont.classList.add('block');
+    
+                        finalPoints.innerHTML = counter;
+
+                        verify = 1;
+
+                        containerPoints.classList.add('none')
+    
+                    } else if (!myCell.classList.contains("cell-color")) {
+                    
+                        myCell.classList.add('cell-color');
+    
+                        counter++;
+    
+                        myPoints.innerHTML = counter;
+
+                    }
+                    
+                    myElement.classList.toggle('none');
+        
+                    console.log(i);
+
+                }
+    
+            }
+
+        );        
+    
+    };
+
+};
+
+//funzione random number
+function randomNumber (min ,max) {
+    
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+};
+
+// funzione per generare i 16 numeri
+function myRandomNumberBomb (num1, num2, myArray) {
+
+    for (let i = 1; i <= 16; i++) {
+
+        let myRandomNumber = randomNumber (num1 ,num2);
+    
+        while (myArray.includes(myRandomNumber)) {
+    
+            myRandomNumber = randomNumber (num1 ,num2);
+    
+        };
+    
+        myArray.push(myRandomNumber);
+    
+        console.log(myRandomNumber)
+    
+    };
+
+};
